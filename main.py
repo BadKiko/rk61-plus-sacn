c12f21c7e0efa028e14f659f2a3024bea0b193e3
【﻿Ｋｉｋｏ】 <53761278+BadKiko@users.noreply.github.com>
2024-04-22 04:59:39 +0300

    [i] init

--- main.py
+++ main.py
@@ -8,21 +8,21 @@ import queue
 import threading
 import platform
 
-RK84_VID = 0x258a
-RK84_PID = 0xc8
+RK61_PLUS_VID = 0x258a
+RK61_PLUS_PID = 0xf8
 IP = "127.0.0.84"
 
 receiver = sacn.sACNreceiver(IP)
-rk84 = hid.enumerate(RK84_VID, RK84_PID)
+rk61 = hid.enumerate(RK61_PLUS_VID, RK61_PLUS_PID)
 h = hid.device()
-packets_queue = queue.Queue()
+packets_queue = queue.Queue(5)
 packet_sender_stop_event = threading.Event()
 last_packets: list[bytes] = []
 
 @receiver.listen_on('universe', universe=1)
 def sacn_callback(packet: sacn.DataPacket):
     global last_packets
-    
+
     dmx_data = packet.dmxData
     colors = []
 
@@ -34,14 +34,15 @@ def sacn_callback(packet: sacn.DataPacket):
 
     # print("sending packets:")
     for packet in packets:
-        packets_queue.put(packet)
+        if packets_queue.not_full:
+            packets_queue.put(packet)
 
     last_packets = packets
 
 def usb_packet_sender():
     while not packet_sender_stop_event.is_set():
         try:
-            packet = packets_queue.get(block=True, timeout=1)
+            packet = packets_queue.get()
             # print(packet.hex(" "))
             h.send_feature_report(packet)
         except queue.Empty:
@@ -55,17 +56,17 @@ def usb_packet_sender():
 if __name__ == "__main__":
     rk_path = None
     if platform.system() == "Windows":
-        for interface in rk84:
+        for interface in rk61:
             # pprint.pprint(interface)
             if interface['usage_page'] == 65280:
                 rk_path = interface['path']
                 # break
     elif platform.system() == "Linux":
-        if len(rk84) >=2:
-            rk_path = rk84[1]['path']
+        if len(rk61) >=2:
+            rk_path = rk61[1]['path']
 
     if rk_path is None:
-        print("RK84N not found")
+        print("rk61-plus not found")
         exit(1)
 
     h.open_path(rk_path)
